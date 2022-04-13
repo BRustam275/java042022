0f1e6a89256511f05f7a7e607eeff6049422ae04
Rustam <r.bagautdinov275@gmail.com>
2022-04-13 22:30:53 +0500

    Task 2

--- src/main/java/ru/gb/bagautdinov/HomeWork1/Task2.java
+++ src/main/java/ru/gb/bagautdinov/HomeWork1/Task2.java
@@ -1,7 +1,13 @@
 package ru.gb.bagautdinov.HomeWork1;
 
 public class Task2 {
+    public static void printThreeWords(String text) {
+        System.out.println(text);
+    }
     public static void main(String[] args) {
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
 
     }
 }
--- src/main/java/ru/gb/bagautdinov/HomeWork1/Task3.java
+++ src/main/java/ru/gb/bagautdinov/HomeWork1/Task3.java
@@ -1,7 +1,7 @@
 package ru.gb.bagautdinov.HomeWork1;
 
 public class Task3 {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void checkSumSign() {
+
     }
 }
