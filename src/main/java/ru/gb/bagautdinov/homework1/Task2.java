2cb6a3e3837da359fc7f2e5bee2a2aae85d35a43
Rustam <r.bagautdinov275@gmail.com>
2022-04-14 16:17:47 +0500

    Completed task 2

---
+++ src/main/java/ru/gb/bagautdinov/homework1/Task2.java
@@ -0,0 +1,13 @@
+package ru.gb.bagautdinov.homework1;
+
+public class Task2 {
+    public static void printThreeWords(String text){
+        System.out.println(text);
+    }
+
+    public static void main(String[] args) {
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
+    }
+}
